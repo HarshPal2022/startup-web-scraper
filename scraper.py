3edfc0aa23da5621b3b5707417e2db45c76435db
Harsh Ravikant Pal <palharrav21bcs@student.mes.ac.in>
2026-03-16 00:17:27 +0530

    final updates

--- scraper.py
+++ scraper.py
@@ -6,23 +6,36 @@ import time
 driver = webdriver.Chrome()
 
 url = "https://www.ycombinator.com/companies"
-
 driver.get(url)
 
 time.sleep(5)
 
-companies = driver.find_elements(By.CLASS_NAME, "_company_i9oky_355")
+for i in range(6):
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(2)
+
+companies = driver.find_elements(By.CSS_SELECTOR,"a[href^='/companies/']")
 
 data = []
 
-for company in companies:
-    name = company.text
-    data.append({"company": name})
+for c in companies:
+    text = c.text.split("\n")
 
-df = pd.DataFrame(data)
+    if len(text) >= 3:
+        name = text[0]
+        location = text[1]
+        description = text[2]
+
+        data.append({
+            "company": name,
+            "location": location,
+            "description": description
+        })
+
+df = pd.DataFrame(data).drop_duplicates()
 
 df.to_csv("startups.csv", index=False)
 
-print("Startup data saved!")
+print("Saved", len(df), "companies")
 
 driver.quit()
